ce03199bb454c5bdaafbb4c5cee863afdad63ac8
Alberto Alexis Dominguez Ruiz <t1mk1ll3r4ll@gmail.com>
2021-07-08 16:08:47 -0500

    1st commit

---
+++ pages/index.tsx
@@ -0,0 +1,142 @@
+import React from 'react'
+//import '../stylesheets/style.css'
+
+const index = () => {
+  return (
+    <div>
+        <title>
+          Trade
+        </title>
+        <header>
+          <div className="links">
+          <a>Link 1 </a>
+          <a>Link 2 </a>
+          <a>Link 3 </a>
+          <a>Link 4 </a>
+          </div>
+          <div className="logo"><h1>Trade</h1></div>
+          <button type="submit" className="button">try now</button>
+        </header>
+        <div className="BigText">
+            <h1>
+              Your Best Value Position
+            </h1>
+        </div>
+
+          <div className="smallText">
+          <a>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</a>
+          </div>
+
+          <div className="startfree">
+            <button type="submit">Start Free Trial</button>
+          </div>
+
+        <div className="roundRectangle"/>
+        <div className="divide">
+          <h2>
+            the publication
+          </h2>
+          <a>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </a>
+        </div>
+        <div className="container">
+          <div/>
+          <div/>
+          <div/>
+          <div/>
+          <div/>
+        </div>
+
+        <div className="blackContainer">
+          <div className="divBlac">
+            <div></div>
+          </div>
+          <div className="divWhite">
+            <h2>
+              Feature that is amazing
+            </h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <button>
+              Learn more
+            </button>
+          </div>
+        </div>
+        <div className="greyContainer">
+          
+          <div className="divWhite">
+            <h2>
+              Feature that is amazing
+            </h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <button>
+              Learn more
+            </button>
+          </div>
+          <div className="divGrey">
+            <div></div>
+          </div>
+        </div>
+        <div className="blackContainer">
+          <div className="divBlac">
+            <div></div>
+          </div>
+          <div className="divWhite">
+            <h2>
+              Feature that is amazing
+            </h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <button>
+              Learn more
+            </button>
+          </div>
+        </div>
+        <div className="valuePropContainer">
+          <h2>
+            Your best value proposition
+          </h2>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          </p>
+          <div className="containerProposition">
+            <div>
+              <div className="proposition">
+                <a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                </a>
+                <a>
+                </a>
+              </div>
+            </div>
+            <div>
+              <div className="proposition">
+                <a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                </a>
+                <a>
+                </a>
+              </div>
+            </div>
+            <div>
+              <div className="proposition">
+                <a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                </a>
+                <a>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default index
