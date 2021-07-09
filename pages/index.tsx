b582c490b911360979f1dc6df72d41556f7ce7d2
Alberto Alexis Dominguez Ruiz <t1mk1ll3r4ll@gmail.com>
2021-07-08 21:38:08 -0500

    asdasdasd

--- pages/index.tsx
+++ pages/index.tsx
@@ -105,36 +105,58 @@ const index = () => {
           <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
           </p>
-          <div className="containerProposition">
-            <div>
-              <div className="proposition">
-                <a>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                </a>
-                <a>
-                </a>
-              </div>
-            </div>
-            <div>
-              <div className="proposition">
-                <a>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                </a>
-                <a>
-                </a>
-              </div>
-            </div>
-            <div>
-              <div className="proposition">
-                <a>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                </a>
-                <a>
-                </a>
-              </div>
-            </div>
-          </div>
         </div>
+          <div className="containerProposition">
+              <div>
+              </div>
+              <div>
+              </div>
+              <div>
+              </div>
+            </div>
+
+            <div className="lettersProposition">
+              <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+              <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+              <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+            </div>
+            <div className="lettersPropositiona">
+              <a>Learn More</a>
+              <a>Learn More</a>
+              <a>Learn More</a>
+            </div>
+            <div className="whiteBanner">
+              <h1>
+              “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.”
+              </h1>
+              <div>
+              </div>
+              <a>Maria Lopez, VP of Design at Meshery</a>
+            </div>
+            <div className="greyBanner">
+              <div>
+                <h1>
+                Ready to get started?
+                </h1>
+                <p>Sign up or Contact us</p>
+              </div>
+
+              <div className="separator">
+                <button className="ButtonBlack">
+                  Start Free Trial
+                </button>
+                <button className="buttonWhite">
+                  Contact Us
+                </button>
+              </div>
+            </div>
+           
+             <div className="lineSeparator">
+               <div></div>
+             </div>
+            <div className="footer">
+
+            </div>
     </div>
   )
 }
